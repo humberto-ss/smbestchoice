cc7681603894769f114c8428f96d5942d078a2c2
Humberto Silveira <humberto_ss@hotmail.com>
2021-10-02 16:25:18 -0300

    Categorie Domain

--- src/main/java/com/supermarket/bestchoice/resources/CategorieResource.java
+++ src/main/java/com/supermarket/bestchoice/resources/CategorieResource.java
@@ -1,15 +1,27 @@
 package com.supermarket.bestchoice.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.supermarket.bestchoice.domain.Categorie;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategorieResource {
 	
-	@GetMapping
-	public String list() {
-		return "Rest is working";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categorie> list() {
+		Categorie cat1 = new Categorie(1, "Fruits");
+		Categorie cat2 = new Categorie(2, "Vegetables");
+		
+		List<Categorie>listCategories = new ArrayList<>();
+		listCategories.add(cat1);
+		listCategories.add(cat2);
+		
+		return listCategories;
 	}
 }
